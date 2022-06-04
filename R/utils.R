8b7561cb7da2599f0e3cd51b2d4ad16c06063a21
Eve Perry <eve@eveperry.com>
2022-06-03 23:14:15 -0700

    function to check stata path

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,31 @@
 stata_path = function() {
   path = getOption("statar.stata.path", Sys.getenv("STATAR_STATA_PATH"))
 
+  check_stata_path(path, rlang::caller_env())
+
   return(path)
+}
+
+check_stata_path = function(path, call = rlang::caller_env()) {
+  if (path == "") {
+    cli::cli_abort(
+      c(
+        "Path to Stata installation must be provided.",
+        i = "Provide the path in either:",
+        i = "R option 'statar.stata.path'",
+        i = "environmental variable 'STATAR_STATA_PATH'."
+      ),
+      call = call
+    )
+  }
+
+  if (!dir.exists(path)) {
+    cli::cli_abort(
+      c(
+        "Stata installation directory does not exist.",
+        x = "You provided {path}"
+      ),
+      call = call
+    )
+  }
 }
